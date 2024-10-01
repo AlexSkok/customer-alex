099a42de7028cccd8c5006cd8df13c040ba72e7a
Alex <olexiyskok@gmail.com>
2024-10-01 23:29:26 +0300

    app

--- src/main/java/app/Customer.java
+++ src/main/java/app/Customer.java
@@ -1,2 +1,17 @@
-package app;public class Customer {
+package app;
+
+public class Customer {
+    public static String name;
+    public static String phone;
+
+    public Customer(String name, String phone){
+        this.name = name;
+        this.phone = phone;
+    }
+    public String getName(){
+         return name;
+    }
+    public String getPhone(){
+        return phone;
+    }
 }
