c825cfd3ef8754bfd977fb6fa40a1641ccde9b24
Alex <olexiyskok@gmail.com>
2024-10-02 20:37:58 +0300

    my variant of homework

--- src/main/java/app/Customer.java
+++ src/main/java/app/Customer.java
@@ -4,14 +4,17 @@ public class Customer {
     public static String name;
     public static String phone;
 
-    public Customer(String name, String phone){
+    public Customer(String name, int phone){
         this.name = name;
-        this.phone = phone;
+        this.phone = toString(phone);
     }
     public String getName(){
          return this.name;
     }
     public String getPhone(){
         return this.phone;
+    }
+    public static String toString(int info){
+        return String.valueOf(info);
     }
 }
